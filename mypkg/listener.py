72bd6d3ebb241c2599dff697c2e2f2d4c30f7309
sasaki <s21c1053mt@s.chibakoudai.jp>
2022-11-24 13:01:03 +0900

    Initial commit

---
+++ mypkg/listener.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2022 Yuto Sasaki
+# SPDX-License-Identifier: BSD-3-Clause
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Person
+
+def cb(msg):
+    global node
+    node.get_logger().info("Listen: %s" % msg)
+    
+rclpy.init()
+node = Node("listener")
+pub = node.create_subscription(Person, "person", cb, 10)
+
+rclpy.spin(node)
