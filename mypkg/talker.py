f2d47ce5ca66b976d16bd8767e7da5e3f4c1204c
sasaki <s21c1053mt@s.chibakoudai.jp>
2022-12-08 17:49:00 +0900

    Add a command

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -8,7 +8,7 @@ from rclpy.node import Node
 from std_msgs.msg import Int16   
 
 class Talker():
-    def _init_(self, node):
+    def __init__(self, node):
         self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
         node.create_timer(0.5, self.cb)
